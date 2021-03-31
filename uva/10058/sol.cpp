ed7191d58ce7af7b445dc0eba94a8934604dcea7
25077667 <zxc25077667@gmail.com>
2021-03-31 14:45:24 +0800

    Chage const str to literal

--- uva/10058/sol.cpp
+++ uva/10058/sol.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const string NOUN("(tom|jerry|goofy|mickey|jimmy|dog|cat|mouse)");
-const string VERB("(hate|love|know|like)s*");
-const string ARTICLE("(a|the)");
-const string ACTOR = "(" + NOUN + "|" + ARTICLE + " " + NOUN + ")";
-const string ACTIVE_LIST = "(" + ACTOR + " and )*" + ACTOR;
-const string ACTION = ACTIVE_LIST + " " + VERB + " " + ACTIVE_LIST;
-const string STATEMENT = ACTION + "( , " + ACTION + ")*";
+#define NOUN "(tom|jerry|goofy|mickey|jimmy|dog|cat|mouse)"
+#define VERB "(hate|love|know|like)s*"
+#define ARTICLE "(a|the)"
+#define ACTOR  "(" NOUN "|" ARTICLE " " NOUN ")"
+#define ACTIVE_LIST "(" ACTOR " and )*" ACTOR
+#define ACTION  ACTIVE_LIST " "  VERB  " " ACTIVE_LIST
+#define STATEMENT  ACTION "( , " ACTION ")*"
+// string macros
+
 
 int main()
 {
     regex re(STATEMENT);
     string line;
     while (getline(cin, line)) {
-        stringstream ss;
-        ss << line;
         if (regex_match(line.data(), re))
             cout << "YES I WILL" << endl;
         else
